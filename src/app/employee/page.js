732e3903b87929f458075938cf036f6fdb1a81b5
fsDevLegacyCode <86556234+fsDevLegacyCode@users.noreply.github.com>
2024-03-10 15:10:25 +0000

    forms and tables

---
+++ src/app/employee/page.js
@@ -0,0 +1,80 @@
+'use client'
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+import { useSearchParams } from 'next/navigation'
+
+
+export default function Employee(){
+
+
+    const searchParams = useSearchParams()
+    const id = searchParams.get('id')
+
+    const [formDataClass, setFormDataClass] = useState({name:'',brand:'',plate:''});
+
+    // Function to handle form field changes
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormDataClass(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+        }
+    
+    const update = async ()=>{
+        try {
+            const response = await axios.post('url',formDataClass);
+            alert(response.data.message);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const redirectUser = ()=>{
+      window.location.href = `/employees`;
+    }
+
+    useEffect(() => {
+        const getUser = async () => {
+            try {
+                const response = await axios.get('https://dummyjson.com/users/' + id);
+                const userData = response.data;
+                setFormDataClass({
+                    name: userData.firstName,
+                    surname: userData.lastName,
+                    email: userData.email
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        };
+    
+        getUser();
+    }, [id]);
+
+
+
+
+    return(
+     
+        <div className="content">
+        <div><h1>Seja Bem Vindo ao Sistema de Gestão V1.0</h1></div>
+        <div className="formulary">
+          <div><h3>Dados do Empregado</h3></div>
+          <form>
+            <label>Username:</label><input name="name" type="text" value={formDataClass.name} onChange={handleInputChange} placeholder="Insert Your Name" />
+            <label>Surname:</label><input name="surname" type="text" value={formDataClass.surname} onChange={handleInputChange} placeholder="Insert Car Brand" />
+            <label>Email:</label><input name="email" type="text" value={formDataClass.email} onChange={handleInputChange} placeholder="Insert Car Plate" />
+
+                <div className="formButtons">
+                  <button type="button" onClick={redirectUser}>Voltar</button>
+                  <button type="button" onClick={update}>Salvar</button>
+                </div>
+            </form>
+          </div>
+        
+
+        
+      </div>
+    );
+}
--- src/app/employees/page.js
+++ src/app/employees/page.js
@@ -6,7 +6,21 @@ import axios from 'axios';
 
 export default function Employees(){
 
+    const [name,setName]=useState("");
     const [users,setUsers] = useState([]);
+    
+    
+    const searchUsers = async () => {
+      
+      try {
+        const response = await axios.get("https://dummyjson.com/users/search?q="+name);
+        setUsers(response.data.users);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    
     useEffect(() => {
     const getUsers = async ()=>{
       try {
@@ -18,12 +32,27 @@ export default function Employees(){
     }
     getUsers();
   }, []);
+
+    const enterPress = (e) => {
+      if(e.key === 'Enter'){
+        searchUsers();
+      }
+    }
+
+    const redirectUser = (userId)=>{
+      window.location.href = `/employee?id=${userId}`;
+    }
+
+
+
+
     return(
      
         <div className="content">
         <div><h1>Seja Bem Vindo ao Sistema de Gestão V1.0</h1></div>
         <div className="formulary">
           <div><h3>Lista de Empregados</h3></div>
+          <div><input name="name" value={name} onChange={(e)=>{setName(e.target.value)}} onKeyUp={enterPress} placeholder="Search for a username"></input></div>
               <div>
               <table>
                 <thead>
@@ -36,9 +65,10 @@ export default function Employees(){
                   </tr>
                 </thead>
                 <tbody>
-                  {users ? 
+                  {users ?  
                     users.map(user => (
-                      <tr key={user.id}>
+                      
+                      <tr key={user.id} onClick={() => redirectUser(user.id)}>
                         <td>{user.id}</td>
                         <td>{user.firstName+" "+user.lastName}</td>
                         <td>{user.age}</td>
@@ -47,9 +77,7 @@ export default function Employees(){
                       </tr>
                     )) 
                     : 
-                    <tr>
-                      <td colSpan="5">Loading...</td>
-                    </tr>
+                    <div>Loading...</div>
                   }
                 </tbody>
               </table>
